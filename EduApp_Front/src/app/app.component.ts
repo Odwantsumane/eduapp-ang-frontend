c7fddb740dea375208dfba8028fe83674c1c3c46
Odwantsumane <odwantsumane@gmail.com>
2024-10-25 18:37:13 +0200

    moved all code from app-comp to its respective component

--- EduApp_Front/src/app/app.component.ts
+++ EduApp_Front/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HorizontalNavBarComponent } from './components/horizontal-nav-bar/horizontal-nav-bar.component';
+import { VerticalNavBarComponent } from './components/vertical-nav-bar/vertical-nav-bar.component';
+import { ChatsViewComponent } from './components/chats-view/chats-view.component';
+import { ArticlesComponent } from './components/articles/articles.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HorizontalNavBarComponent, VerticalNavBarComponent, ChatsViewComponent,ArticlesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
