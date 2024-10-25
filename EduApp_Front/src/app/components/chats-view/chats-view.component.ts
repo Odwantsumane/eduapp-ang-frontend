1499d5592e1cf13daaa324c999c7d144559cc133
Odwantsumane <odwantsumane@gmail.com>
2024-10-25 18:26:26 +0200

    creating all necessary components

---
+++ EduApp_Front/src/app/components/chats-view/chats-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chats-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './chats-view.component.html',
+  styleUrl: './chats-view.component.css'
+})
+export class ChatsViewComponent {
+
+}
---
+++ EduApp_Front/src/app/components/vertical-nav-bar/vertical-nav-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vertical-nav-bar',
+  standalone: true,
+  imports: [],
+  templateUrl: './vertical-nav-bar.component.html',
+  styleUrl: './vertical-nav-bar.component.css'
+})
+export class VerticalNavBarComponent {
+
+}
