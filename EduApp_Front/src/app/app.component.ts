916c87327eda737c5b45391873e4bcabaed6e23f
Odwantsumane <odwantsumane@gmail.com>
2024-10-29 18:29:27 +0200

    added calendar, and a modal for calendar and a options profile functionality

--- EduApp_Front/src/app/app.component.ts
+++ EduApp_Front/src/app/app.component.ts
@@ -1,17 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HorizontalNavBarComponent } from './components/horizontal-nav-bar/horizontal-nav-bar.component';
 import { VerticalNavBarComponent } from './components/vertical-nav-bar/vertical-nav-bar.component';
 import { ChatsViewComponent } from './components/chats-view/chats-view.component';
 import { ArticlesComponent } from './components/articles/articles.component';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HorizontalNavBarComponent, VerticalNavBarComponent, ChatsViewComponent,ArticlesComponent],
+  imports: [RouterOutlet, HorizontalNavBarComponent, VerticalNavBarComponent, 
+    ChatsViewComponent,ArticlesComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'EduApp_Front';
+  hide_chats : boolean = false;
+
+  hideChats(): void {
+    this.hide_chats ? this.hide_chats = false : this.hide_chats = true; 
+  }
 }
--- EduApp_Front/src/app/components/horizontal-nav-bar/horizontal-nav-bar.component.ts
+++ EduApp_Front/src/app/components/horizontal-nav-bar/horizontal-nav-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CalendarComponent } from '../calendar/calendar.component';
 
 @Component({
   selector: 'app-horizontal-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [CalendarComponent],
   templateUrl: './horizontal-nav-bar.component.html',
   styleUrl: './horizontal-nav-bar.component.css'
 })
