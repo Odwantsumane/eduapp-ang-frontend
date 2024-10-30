9753e2c872c511814cf5335c9168027ec330a3d0
Odwantsumane <odwantsumane@gmail.com>
2024-10-30 21:45:25 +0200

    updating the profile option

--- EduApp_Front/src/app/components/horizontal-nav-bar/horizontal-nav-bar.component.ts
+++ EduApp_Front/src/app/components/horizontal-nav-bar/horizontal-nav-bar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CalendarComponent } from '../calendar/calendar.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-horizontal-nav-bar',
   standalone: true,
-  imports: [CalendarComponent],
+  imports: [CalendarComponent, FormsModule, CommonModule],
   templateUrl: './horizontal-nav-bar.component.html',
   styleUrl: './horizontal-nav-bar.component.css'
 })
