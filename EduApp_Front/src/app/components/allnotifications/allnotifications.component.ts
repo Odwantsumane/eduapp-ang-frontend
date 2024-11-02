02822182b8eedd4ef56c4cf8120f659b9226ebbc
Odwantsumane <odwantsumane@gmail.com>
2024-11-02 14:02:49 +0200

    curating like and dislike buttons

---
+++ EduApp_Front/src/app/components/allnotifications/allnotifications.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-allnotifications',
+  standalone: true,
+  imports: [],
+  templateUrl: './allnotifications.component.html',
+  styleUrl: './allnotifications.component.css'
+})
+export class AllnotificationsComponent {
+
+}
--- EduApp_Front/src/app/components/articles/articles.component.ts
+++ EduApp_Front/src/app/components/articles/articles.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-articles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './articles.component.html',
   styleUrl: './articles.component.css'
 })
 export class ArticlesComponent {
 
+  like(): void { //arg = article-id
+    console.log("liked");
+  }
+
+  dislike(): void { //arg = article-id
+    console.log("disliked");
+  }
 }
