a719762922d7ee5849eaab58286fdba353ef7e2f
Odwantsumane <odwantsumane@gmail.com>
2025-01-19 08:15:19 +0200

    uploading the file setup success

--- EduApp_Front/src/app/components/folder/folder.component.ts
+++ EduApp_Front/src/app/components/folder/folder.component.ts
@@ -1,16 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-folder',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './folder.component.html',
   styleUrl: './folder.component.css'
 })
 export class FolderComponent {
 
   id: string | null = null;
+  file : File | null = null;
+  file_obj = {
+    name:"",
+    file: this.file
+  }
 
   constructor(private route: ActivatedRoute) {}
 
@@ -18,7 +25,21 @@ export class FolderComponent {
     this.id = this.route.snapshot.paramMap.get('id'); // Get the 'id' from the URL 
   }
 
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const selectedFile = input.files[0];
+      this.file_obj.file = selectedFile;
+      this.file_obj.name = selectedFile.name; 
+      this.file = selectedFile;
+    }
+  }
+
   getAllFiles() {
     
+  }
+
+  uploadFile() {
+    console.log(this.file);
   }
 }
