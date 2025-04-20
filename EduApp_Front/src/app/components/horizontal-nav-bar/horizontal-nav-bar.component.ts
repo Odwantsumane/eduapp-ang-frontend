f1ee76eec4dd2f4ce0bd36924c2c81da2162aee6
Odwantsumane <odwantsumane@gmail.com>
2025-04-20 19:42:25 +0200

    adjusting the navbar

--- EduApp_Front/src/app/components/horizontal-nav-bar/horizontal-nav-bar.component.ts
+++ EduApp_Front/src/app/components/horizontal-nav-bar/horizontal-nav-bar.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CalendarComponent } from '../calendar/calendar.component';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { User} from '../../services/userrequest.service'; 
+import { ManagerService } from '../../services/Aministration/manager.service';
 
 @Component({
   selector: 'app-horizontal-nav-bar',
@@ -11,6 +13,15 @@ import { RouterLink } from '@angular/router';
   templateUrl: './horizontal-nav-bar.component.html',
   styleUrl: './horizontal-nav-bar.component.css'
 })
-export class HorizontalNavBarComponent {
+export class HorizontalNavBarComponent implements OnInit {
+
+  list_users: Array<User> = [];
+
+  constructor(private userService: ManagerService) {
+
+  }
+  async ngOnInit() {
+    this.list_users = await this.userService.getAllUsers();
+  } 
 
 }
