a273f65442aa1d0e8b6d61c87410cbe7008bd60b
Odwantsumane <odwantsumane@gmail.com>
2025-04-21 20:22:57 +0200

    persisting the showing of public chats and others

--- EduApp_Front/src/app/components/homecontent/homecontent.component.ts
+++ EduApp_Front/src/app/components/homecontent/homecontent.component.ts
@@ -14,9 +14,10 @@ import { VerticalNavBarComponent } from '../vertical-nav-bar/vertical-nav-bar.co
   styleUrl: './homecontent.component.css'
 })
 export class HomecontentComponent {
-  hide_chats : boolean = false;
+  hide_chats : boolean = window.sessionStorage.getItem("show_charts_studyo") === "true" ? true : false;
 
   hideChats(): void {
+    this.hide_chats ? window.sessionStorage.setItem("show_charts_studyo", "false") : window.sessionStorage.setItem("show_charts_studyo", "true")
     this.hide_chats ? this.hide_chats = false : this.hide_chats = true; 
   }
 }
